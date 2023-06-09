4eb64ed7d61e4c9e8fc8849cf92bba7661de8da9
Aleksey_Sokolov <sokolov@student.tugraz.at>
2023-06-10 00:50:37 +0200

    wutang lan

--- Rotary Encoder/rotary_encoder.py
+++ Rotary Encoder/rotary_encoder.py
@@ -32,23 +32,27 @@ Bug1:
 
 class serial_mma_data_stream:
 
-    def __init__(self, port:str, baudrate:int, len:int=10):
+    def __init__(self, port:str, baudrate:int,  rx_size: int,calibration_weight:float, len:int=10):
         self.port = port
         self.baudrate = baudrate
+        self.rx_size = rx_size
         self.conn = serial.Serial(port=self.port, baudrate=self.baudrate)
         self.start = True
         self.len = 10
+        self.calibration_weight = calibration_weight
+        self.calibration_factor = 1
+        self.offset = 0
+        self.conn.setDTR(False)
+        time.sleep(1)
+        self.conn.flushInput()
+        self.conn.set_buffer_size(rx_size = self.rx_size)
+        self.conn.setDTR(True)
 
     def get_next_meas(self) -> bytes:
         numbytes = self.conn.in_waiting
         if(numbytes <=10):
             return None
 
-        if(self.start):
-            data = self.conn.readline()
-            print(''.join(format(x, '02x') for x in data))
-            print("start sequence")
-            start = False
 
         return self.conn.read(self.len)
     
@@ -58,9 +62,23 @@ class serial_mma_data_stream:
             return None
 
         rot_pos = int.from_bytes(meas_data[0:4], byteorder='little')
-        timeVal = int.from_bytes(meas_data[4:8], byteorder='little')
+        rot_timeVal = int.from_bytes(meas_data[4:8], byteorder='little')
 
-        return (timeVal,rot_pos,meas_data)
+        return (rot_timeVal,rot_pos,meas_data)
+    def get_next_loadcell(self) -> tuple():
+        meas_loadcell = self.get_next_meas()
+        if meas_loadcell is None: 
+            return None
+        
+        call_byte = meas_loadcell[0]
+        if call_byte == 128:
+            self.calibration_factor = (float(int.from_bytes(meas_loadcell[1:4]), byteorder = "little"))/self.calibration_weight
+            self.offset = int.from_bytes(meas_loadcell[4:8])
+        else:
+            loadcell_raw = int.from_bytes(meas_loadcell[0:4], byteorder= "little")
+            loadcell_timeVal = int.from_bytes(meas_loadcell[4:8], byteorder= "little")
+            return (loadcell_raw,loadcell_timeVal,meas_loadcell)
+    
 
 
 
@@ -72,7 +90,7 @@ class serial_mma_data_stream:
 #ä    return data
 
 
-arduino = serial_mma_data_stream("COM9", 115200)
+arduino = serial_mma_data_stream("COM9", 57600,16384,500)
 rotPosition_ls = []
 timeVal_ls = []
 #bytes_ls = []
@@ -80,53 +98,77 @@ timeVal = 0
 rot_pos = 0
 databyt = bytes()
 #debug_flag = False
+loadcell_raw_ls= []
+loadcell_converted_ls = []
+loadcell_timeVal_ls = []
 i = 0
-while i < 20:
-    #numbytes = arduino.in_waiting
-    #if numbytes <= 10:
-    #    continue
-    retVal = arduino.get_next_rot_enc()
+for i in range(0,200):
+    
 
-    if(retVal is not None):
-        (timeVal,rot_pos,databyt) = retVal
-        i = i + 1
+    data = arduino.get_next_loadcell()
+    if(data is not None):
+        (loadcell_raw,loadcell_timeVal,val3) = data 
+        print("raw data loadcell:",loadcell_raw,"time_val micros:", loadcell_timeVal, "bytestring:", val3)
+
+        loadcell_raw_ls.append(loadcell_raw)
+        loadcell_timeVal_ls.append(loadcell_timeVal)
+        loadcell_converted_ls.append(loadcell_raw/(arduino.calibration_factor) - arduino.offset)
+       
+for i in loadcell_converted_ls:
+    print(i)   
+
+plt.scatter(loadcell_timeVal_ls,loadcell_converted_ls )
+plt.show()
 
 
-    #bytes_ls.append(numbytes)
-    #while not (numbytes > 10):
-    #    time.sleep(.001)
 
-    #databyt = readRotaryEncoder()
-    
-    #print(databyt) # printing the value
-    
-    #value = int.from_bytes(databyt, byteorder='little')
-    
-    #rot_pos = int.from_bytes(databyt[0:4], byteorder='big')
-    #timeVal = int.from_bytes(databyt[4:8], byteorder='big')
-    
-    #rot_pos = value >> (32 + 16)
-    #if rot_pos != 0:
-    rotPosition_ls.append(rot_pos)
-    #timeVal = (value >> 16) & 0xFFFFFFFF
-    timeVal_ls.append(timeVal)
-    #print(value.bit_length())
-    #print(timeVal,"\t",rot_pos)
-    
-    #print("Databytes: ",''.join(format(x, '02x') for x in databyt))
-    #print("rot_pos:   ",rot_pos," time_pos:    ",timeVal, "numbytes:    ",numbytes)
-    print(rot_pos,";",timeVal,";",''.join(format(x, '02x') for x in databyt))
-    #if(rot_pos >= 2555 and rot_pos <=2565):
-    #    print("Databytes: ",''.join(format(x, '02x') for x in databyt))
-    #    print("rot_pos:   ",rot_pos," time_pos:    ",timeVal, "numbytes:    ",numbytes)
-    #    debug_flag = True
-    #elif(debug_flag and rot_pos<=50 and rot_pos != 0):
-    #    print("Databytes: ",''.join(format(x, '02x') for x in databyt))
-    #    print("rot_pos:   ",rot_pos," time_pos:    ",timeVal, "numbytes:    ",numbytes)
-    #elif(debug_flag and rot_pos >= 10):
-    #    debug_flag = False
-        
-    
+#i = 0
+#while i < 20:
+#    #numbytes = arduino.in_waiting
+#    #if numbytes <= 10:
+#    #    continue
+#    retVal = arduino.get_next_rot_enc()
+#
+#    if(retVal is not None):
+#        (timeVal,rot_pos,databyt) = retVal
+#        i = i + 1
+#
+#
+#    #bytes_ls.append(numbytes)
+#    #while not (numbytes > 10):
+#    #    time.sleep(.001)
+#
+#    #databyt = readRotaryEncoder()
+#    
+#    #print(databyt) # printing the value
+#    
+#    #value = int.from_bytes(databyt, byteorder='little')
+#    
+#    #rot_pos = int.from_bytes(databyt[0:4], byteorder='big')
+#    #timeVal = int.from_bytes(databyt[4:8], byteorder='big')
+#    
+#    #rot_pos = value >> (32 + 16)
+#    #if rot_pos != 0:
+#    rotPosition_ls.append(rot_pos)
+#    #timeVal = (value >> 16) & 0xFFFFFFFF
+#    timeVal_ls.append(timeVal)
+#    #print(value.bit_length())
+#    #print(timeVal,"\t",rot_pos)
+#    
+#    #print("Databytes: ",''.join(format(x, '02x') for x in databyt))
+#    #print("rot_pos:   ",rot_pos," time_pos:    ",timeVal, "numbytes:    ",numbytes)
+#    print(rot_pos,";",timeVal,";",''.join(format(x, '02x') for x in databyt))
+#    #if(rot_pos >= 2555 and rot_pos <=2565):
+#    #    print("Databytes: ",''.join(format(x, '02x') for x in databyt))
+#    #    print("rot_pos:   ",rot_pos," time_pos:    ",timeVal, "numbytes:    ",numbytes)
+#    #    debug_flag = True
+#    #elif(debug_flag and rot_pos<=50 and rot_pos != 0):
+#    #    print("Databytes: ",''.join(format(x, '02x') for x in databyt))
+#    #    print("rot_pos:   ",rot_pos," time_pos:    ",timeVal, "numbytes:    ",numbytes)
+#    #elif(debug_flag and rot_pos >= 10):
+#    #    debug_flag = False
+#        
+#    
 
 
 # Oachkatzlscjowaf 082
@@ -135,6 +177,6 @@ while i < 20:
 # rotaryEncoder_arr = np.array(timeVal_ls, rotPosition_ls)
 
 #print(rotaryEncoder_arr)
-plt.scatter(timeVal_ls,rotPosition_ls)
-#plt.scatter(np.asarray(timeVal_ls), np.asarray(bytes_ls)*100)
-plt.show()
+#plt.scatter(timeVal_ls,rotPosition_ls)
+##plt.scatter(np.asarray(timeVal_ls), np.asarray(bytes_ls)*100)
+#plt.show()
