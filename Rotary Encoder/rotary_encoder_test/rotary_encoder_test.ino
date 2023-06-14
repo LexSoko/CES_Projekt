ff9ae2dd09d3112b96435e0b4bcbcb81f872e6c5
Mikromegas <martinferdi22@hotmail.com>
2023-06-14 16:08:28 +0200

    martin sync interrupt tests

--- Rotary Encoder/rotary_encoder_test/rotary_encoder_test.ino
+++ Rotary Encoder/rotary_encoder_test/rotary_encoder_test.ino
@@ -1,40 +1,82 @@
+#include <Encoder.h>
+
+
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+/*                    Global Variables                              */
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 #define DATA_PIN_A 2
 #define DATA_PIN_B 3
 
-#include <Encoder.h>
-#include <util/atomic.h>
+volatile bool syncState = false;
+volatile unsigned long syncTime;
 
+long oldPosition = -999;
+long newPosition = 0;
+unsigned long time = 0;
 
 Encoder rotaryEncoder(DATA_PIN_A, DATA_PIN_B);
 
 
-void setup() {
-  Serial.begin(57600);
-  // Serial.println("Rotary Encoder Test:");
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+/*                    IRS                                           */
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+void syncISR() {
+  syncTime = micros();  // ist so 128 microsekunden schneller als wenn nach der ISR
+  syncState = true;
+
 }
 
-long oldPosition  = -999;
-unsigned long newPosition = 0;
-unsigned long time = 0;
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+/*                    Functions                                     */
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+void syncController(bool sendflag = false, int INTERRUPT_PIN_SYNC = 2, int SYNC_SEND = 4 ) {
+  
+  delay(50);
 
+  if(sendflag == true){
+    pinMode(SYNC_SEND, OUTPUT);
+    digitalWrite(SYNC_SEND, HIGH);
+  }
+
+  pinMode(INTERRUPT_PIN_SYNC, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN_SYNC), syncISR, LOW);
+
+  while (syncState == false){
+    if(sendflag == true){
+      digitalWrite(SYNC_SEND, LOW);
+    }
+  }
+
+  Serial.println(syncTime);
+  // Serial.write((byte*)&syncTime, sizeof(syncTime));
+  // Serial.write((byte*)&syncTime, sizeof(syncTime));
+  // Serial.println();
+
+  delay(1000);
+}
+
+
+
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+/*                    Setup                                         */
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+void setup() {
+  Serial.begin(57600);
+  syncController(true);
+}
+
+
+// Loop
 void loop() {
+
+
+
   newPosition = rotaryEncoder.read();
   time = micros();
 
-  //Serial.write(newPosition>>24);
-  //Serial.write(newPosition>>16);
-  //Serial.write(newPosition>>8);
+  // Serial.write((byte*)&newPosition, sizeof(newPosition));
 
-  Serial.write((byte*) &newPosition, sizeof(newPosition));
-  //Serial.print(newPosition);
-  //Serial.print(";");
-
-  //Serial.write(time>>24);
-  //Serial.write(time>>16);
-  //Serial.write(time>>8);
-
-  Serial.write((byte*)&time, sizeof(time));
-  
-  //Serial.print(time);
+  // Serial.write((byte*)&time, sizeof(time));
+  Serial.println(newPosition);
   Serial.println();
 }
