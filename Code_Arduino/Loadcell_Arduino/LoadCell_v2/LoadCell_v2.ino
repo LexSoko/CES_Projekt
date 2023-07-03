4bd6a55affc6e4c20b0861ccdc29639b9db8a37e
Aleksey_Sokolov <sokolov@student.tugraz.at>
2023-07-03 06:10:15 +0200

    arduinos seitg bissl loadcell

---
+++ Code_Arduino/Loadcell_Arduino/LoadCell_v2/LoadCell_v2.ino
@@ -0,0 +1,115 @@
+#include "HX711.h"
+
+#define PIN_DT 2
+#define PIN_SCK 3 
+#define BTN_CAL 8
+#define SYNC_PIN 7
+
+
+long raw_offset;  //value that determines the offset in the calibration (tare)
+long raw_calibration; //value with applied force to determine the calibration factor
+unsigned long time_now = 0; // timestamp to send
+long loadcell_read; //value send to python in the work loop
+char commandbuffer[256]; 
+
+byte controll_byte;
+byte  exit = 0x00;
+unsigned long previous_time = 0;
+const unsigned long wait_time = 5000;
+
+
+//variable scale datentyp HX711
+HX711 loadcell;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+  loadcell.begin(PIN_DT,PIN_SCK,64);
+  attachInterrupt(digitalPinToInterrupt(SYNC_PIN), syncInterrupt, FALLING);
+
+  
+  
+  
+  pinMode(BTN_CAL,INPUT_PULLUP);
+  
+  
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available()) {
+    controll_byte = Serial.read();
+  }
+  
+// Work function is initiated using the first bit of the read byte (1000 0000)
+  if (controll_byte == 0x80) {
+    //as long the exit command is not read the work loop continiuos
+    while (exit != 0xF0) {
+      work(); 
+      // checks every five seconds if data is in buffer
+      if (time_now - previous_time >= wait_time) {
+        if (Serial.available())> 0{
+          // sets exit to incoming byte
+          // only if exit is 1111 0000 the loop will be left
+          exit = Serial.read();
+        }
+      }
+    }
+  }
+  // controll sequence is initiated using 1100 0000
+  if (controll_byte == 0xC0) {
+    calibration();
+  }
+  //sync sequence is initiated using 1110 0000
+  if (controll_byte == 0xE0) {
+    sync();
+  }
+}
+
+void work() {
+  loadcell_read = loadcell.read();
+  time_now = millis();
+  
+  Serial.write((byte*) &loadcell_read, sizeof(loadcell_read));
+  Serial.write((byte*) &time_now, sizeof(time_now));
+  Serial.println();
+    
+}
+void syncInterrupt(){
+  time_now = micros()
+  exit = 0xF0;
+}
+void sync() {
+  exit = 0x00;
+  while(exit != 0xF0){
+    //just waits for interrupt
+  }
+}
+void calibration() {
+  // stays in loop as long the commands are not given
+  exit = 0x00;
+  while (exit != 0xF0) {
+    //only triggers if tare command is given 0000 1010
+    if (Serial.read == 0x0A){
+      // measuring the offset
+      raw_offset = loadcell.read_average(50);
+      exit = 0xF0;
+    }
+  }
+
+  exit = 0x00;
+  while (exit != 0xF0) {
+     //only triggers if calibration raw value command is given 0000 1011
+    if (Serial.read == 0x0B){
+      //measuring the raw value after the calibration weight is placed
+      raw_calibration = loadcell.read_average(50);
+      exit = 0xF0;
+    }
+  }
+  //when both values are read the arduino sends them as 10 byte strings
+  raw_offset = raw_offset | 0x80000000;
+  Serial.write((byte*) &raw_offset, sizeof(raw_offset));
+  Serial.write((byte*) &raw_calibration, sizeof(raw_calibration));
+  Serial.println();
+}
