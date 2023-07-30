32a80d177e2dfaa78ded21b0b5dad35b2876bd65
maxjost <max.jost@student.tugraz.at>
2023-07-30 14:29:13 +0200

    bissl ausw plus skizze

--- Code_Python/auswertung.py
+++ Code_Python/auswertung.py
@@ -4,6 +4,7 @@ import pandas as pd
 import re
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.fft import rfft, rfftfreq
 
 graphicsdir="Gesamtdoku/graphics/"
 dirname = "winder_measurements/auswertung/"
@@ -41,6 +42,19 @@ def filter_df(df:pd.DataFrame, key:str, min_val:float=None, max_val:float=None) 
 def calc_unsicherheit(data):
     data.sem()
 
+
+# dateninterpolation auf constante Zeit für fft
+def interpol(df, time_intervall):
+    d = df.set_index('millis')
+    t = d.index
+    r = pd.Index(np.arange(df["millis"].min(),df["millis"].max(),time_intervall), name=t.name)
+    return df.set_index('millis') \
+            .reindex(t.union(r))\
+                .interpolate('index')\
+                    .loc[r]\
+                        .reset_index()
+
+
 ##### allgemeine berechnungen  #####
 latex_dict = {
     "F_WZ":r"$F_{WZ}$ / mN",    #N
@@ -50,6 +64,8 @@ latex_dict = {
     "omeg_NA":r"$\omega_{NA}$ / ° s$^{-1}$",
     "alph_HA":r"$\alpha_{HA}$ / ° s$^{-2}$",   #degrees / (millis)^2
     "alph_NA":r"$\alpha_{NA}$ / ° s$^{-2}$",
+    "amp":r"Amplitude $F_{WZ}(f)$ / willkürliche Einheiten",
+    "freq":r"$f$ / Hz",
 }
 
 for meas in df_dict.values():
@@ -67,24 +83,26 @@ for meas in df_dict.values():
     meas["alph_NA"] = calc_deriv(meas["omeg_NA"],meas["millis"],1)
 
 
-##### auswertung 1 const speed zylinderspule für 25 und 50 speed und für mit und ohne feder
+# zeiten konstanter geschwindigkeit 
 t_intervals_const_speed = {
     "50_no_spring":[47000,75500],
     "25_no_spring":[133500,173000],
     "50_yes_spring":[57500,87000],
     "25_yes_spring":[203000,242000],
+    "quad_no_spring":[135000,199000],
+    "quad_yes_spring":[90000,140000],
 }
 
 df_const_speed_no_spring_50 = filter_df(df_dict["mess1"],"millis",t_intervals_const_speed["50_no_spring"][0],t_intervals_const_speed["50_no_spring"][1])
 df_const_speed_no_spring_25 = filter_df(df_dict["mess1"],"millis",t_intervals_const_speed["25_no_spring"][0],t_intervals_const_speed["25_no_spring"][1])
 df_const_speed_yes_spring_50 = filter_df(df_dict["mess2"],"millis",t_intervals_const_speed["50_yes_spring"][0],t_intervals_const_speed["50_yes_spring"][1])
 df_const_speed_yes_spring_25 = filter_df(df_dict["mess2"],"millis",t_intervals_const_speed["25_yes_spring"][0],t_intervals_const_speed["25_yes_spring"][1])
+df_const_speed_no_spring_50_quad = filter_df(df_dict["mess3"],"millis",t_intervals_const_speed["quad_no_spring"][0],t_intervals_const_speed["quad_no_spring"][1])
+df_const_speed_yes_spring_50_quad = filter_df(df_dict["mess4"],"millis",t_intervals_const_speed["quad_yes_spring"][0],t_intervals_const_speed["quad_yes_spring"][1])
 
-#df_const_speed_no_spring_50.plot.scatter(x="omeg_HA",y="F_WZ",title="no_spring_50")
-#df_const_speed_no_spring_25.plot.scatter(x="omeg_HA",y="F_WZ",title="no_spring_25")
-#df_const_speed_yes_spring_50.plot.scatter(x="omeg_HA",y="F_WZ",title="yes_spring_50")
-#df_const_speed_yes_spring_25.plot.scatter(x="omeg_HA",y="F_WZ",title="yes_spring_25")
 
+
+##### auswertung 1 const speed zylinderspule für 25 und 50 speed und für mit und ohne feder
 # calc mean Fs
 F_m_50_no = df_const_speed_no_spring_50["F_WZ"].mean()
 F_m_25_no = df_const_speed_no_spring_25["F_WZ"].mean()
@@ -107,29 +125,66 @@ omeg_u_25_no =  df_const_speed_no_spring_25["omeg_HA"].std()
 omeg_u_50_yes = df_const_speed_yes_spring_50["omeg_HA"].std()
 omeg_u_25_yes = df_const_speed_yes_spring_25["omeg_HA"].std()
 
-#print(F_m_50_no,F_u_50_no,omeg_m_50_no,omeg_u_50_no)
-#print(F_m_25_no,F_u_25_no,omeg_m_25_no,omeg_u_25_no)
-#print(F_m_50_yes,F_u_50_yes,omeg_m_50_yes,omeg_u_50_yes)
-#print(F_m_25_yes,F_u_25_yes,omeg_m_25_yes,omeg_u_25_yes)
+
+#fig_const = plt.figure(figsize=(9,9))
+#ax_const = fig_const.add_subplot()
 #
+#ax_const.scatter(df_const_speed_no_spring_50["omeg_HA"],df_const_speed_no_spring_50["F_WZ"],c="red",alpha=0.05)
+#ax_const.scatter(df_const_speed_no_spring_25["omeg_HA"],df_const_speed_no_spring_25["F_WZ"],c="red",alpha=0.05)
+#ax_const.scatter(df_const_speed_yes_spring_50["omeg_HA"],df_const_speed_yes_spring_50["F_WZ"],c="green",alpha=0.05)
+#ax_const.scatter(df_const_speed_yes_spring_25["omeg_HA"],df_const_speed_yes_spring_25["F_WZ"],c="green",alpha=0.05)
+#
+#ax_const.errorbar(omeg_m_50_no, F_m_50_no, xerr=omeg_u_50_no, yerr =F_u_50_no, label="no spring", c="red")
+#ax_const.errorbar(omeg_m_25_no, F_m_25_no, xerr=omeg_u_25_no, yerr =F_u_25_no, label="no spring", c="red")
+#ax_const.errorbar(omeg_m_50_yes, F_m_50_yes, xerr=omeg_u_50_yes, yerr =F_u_50_yes, label="with spring" ,c="green")
+#ax_const.errorbar(omeg_m_25_yes, F_m_25_yes, xerr=omeg_u_25_yes, yerr =F_u_25_yes, label="with spring" ,c="green")
+#
+#ax_const.set_xlabel(latex_dict["omeg_HA"])
+#ax_const.set_ylabel(latex_dict["F_WZ"])
+#ax_const.legend()
+#ax_const.grid()
+#
+#
+#fig_const.savefig(graphicsdir+"const_speed.pdf",dpi="figure")
 
-fig_const = plt.figure(figsize=(9,9))
-ax_const = fig_const.add_subplot()
 
-ax_const.scatter(df_const_speed_no_spring_50["omeg_HA"],df_const_speed_no_spring_50["F_WZ"],c="red",alpha=0.05)
-ax_const.scatter(df_const_speed_no_spring_25["omeg_HA"],df_const_speed_no_spring_25["F_WZ"],c="red",alpha=0.05)
-ax_const.scatter(df_const_speed_yes_spring_50["omeg_HA"],df_const_speed_yes_spring_50["F_WZ"],c="green",alpha=0.05)
-ax_const.scatter(df_const_speed_yes_spring_25["omeg_HA"],df_const_speed_yes_spring_25["F_WZ"],c="green",alpha=0.05)
-
-ax_const.errorbar(omeg_m_50_no, F_m_50_no, xerr=omeg_u_50_no, yerr =F_u_50_no, label="no spring", c="red")
-ax_const.errorbar(omeg_m_25_no, F_m_25_no, xerr=omeg_u_25_no, yerr =F_u_25_no, label="no spring", c="red")
-ax_const.errorbar(omeg_m_50_yes, F_m_50_yes, xerr=omeg_u_50_yes, yerr =F_u_50_yes, label="with spring" ,c="green")
-ax_const.errorbar(omeg_m_25_yes, F_m_25_yes, xerr=omeg_u_25_yes, yerr =F_u_25_yes, label="with spring" ,c="green")
-
-ax_const.set_xlabel(latex_dict["omeg_HA"])
-ax_const.set_ylabel(latex_dict["F_WZ"])
-ax_const.legend()
-ax_const.grid()
+##### auswertung 2 ffts für auswirkungen feder
+# datensätze:
+#  mess1 und 2 mit speed 50
+TIME_INTERVAL_INTERPOL = 12.5           #ms
+zyl_no__fed = interpol(df_const_speed_no_spring_50,TIME_INTERVAL_INTERPOL)
+zyl_yes_fed = interpol(df_const_speed_yes_spring_50,TIME_INTERVAL_INTERPOL)
+quad_no__fed = interpol(df_const_speed_no_spring_50_quad,TIME_INTERVAL_INTERPOL)
+quad_yes_fed = interpol(df_const_speed_yes_spring_50_quad,TIME_INTERVAL_INTERPOL)
 
 
-fig_const.savefig(graphicsdir+"const_speed.pdf",dpi="figure")
+fig_ffts = plt.figure(figsize=(9,9))
+legenden = ["zyl. ohne Feder","zyl. mit Feder","quad. ohne Feder","quad. mit Feder"]
+
+for i,df in enumerate([zyl_no__fed, zyl_yes_fed, quad_no__fed, quad_yes_fed]):
+    N = len(df.index)
+    df["amp"] = abs(rfft(df["mN_load"].values))
+    df["amp"] = df["amp"] / df["amp"].max()
+
+    df["freq"] = rfftfreq(N, TIME_INTERVALL/1000)
+
+    ax_ffts = fig_ffts.add_subplot(221+i)
+    ax_ffts.plot(df["freq"],df["amp"],label=legenden[i])
+
+
+ax_ffts.set_xlabel(latex_dict["omeg_HA"])
+ax_ffts.set_ylabel(latex_dict["F_WZ"])
+ax_ffts.legend()
+#ax_ffts.grid()
+
+
+
+
+
+
+#  mess3 und 4 mit speed 50 
+
+
+# daten interpolieren
+
+plt.show()
